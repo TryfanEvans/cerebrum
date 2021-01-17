84263752d4f623c7a5f6765fb1c76c4110f6a376
TryfanEvans <TryfanLE@gmail.com>
2021-01-17 19:04:10 +1030

    init

---
+++ build/precache-manifest.728244eedccdb343db8f697fbfa36961.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a049f049f9a6cd9ce7a",
+    "url": "/css/app.19c621cf.css"
+  },
+  {
+    "revision": "8c9816a50821397317cc",
+    "url": "/css/chunk-vendors.bd588a2a.css"
+  },
+  {
+    "revision": "b5cb06e353b9f699952c4da1e79498a5",
+    "url": "/img/logo.b5cb06e3.svg"
+  },
+  {
+    "revision": "1f87623064487cedadea6f80505c4a56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a049f049f9a6cd9ce7a",
+    "url": "/js/app.11f884f6.js"
+  },
+  {
+    "revision": "8c9816a50821397317cc",
+    "url": "/js/chunk-vendors.505b7968.js"
+  },
+  {
+    "revision": "73d286eba27115a3f55a6b4e7a4ef712",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "359ddd287f4a92b9093b4eeea43e1ea2",
+    "url": "/robots.txt"
+  }
+]);
